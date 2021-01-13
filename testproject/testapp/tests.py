8588a81ac5317df97f5caec36c9329885a1164f6
Egor Skomorokhov <eskomorokhov@rutube.ru>
2021-01-13 19:08:34 +0300

    run pre_sql_setup even if EmptyResultSet

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -104,6 +104,11 @@ class SphinxModelTestCase(SphinxModelTestCaseBase):
         qs = list(self.model.objects.extra(select={'const': 0}, where=['const=0']))
         self.assertEqual(len(qs), 1)
 
+    def testLenOfEmptySet(self):
+        qs = self.model.objects.match("nonexistent")
+        self.assertEqual(qs.count(), 0)
+        self.assertEqual(len(qs[:0]), 0)
+
     def testGroupByExtraSelect(self):
         qs = self.model.objects.all()
 
